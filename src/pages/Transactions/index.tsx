6ae73af4a9ec0151f7ccc3a5e4c25f2a884e9230
Giovanny <didiosilva395@gmail.com>
2023-04-10 12:05:08 -0400

    Adicionado compomente Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
+import { Summary } from "../../Summary";
 import { Header } from "../../components/Header";
 
 export function Transaciotion(){
   return(
     <div>
       <Header/>
+      <Summary/>
     </div>
   )
 }
