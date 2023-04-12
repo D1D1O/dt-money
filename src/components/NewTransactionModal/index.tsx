03a78fce6dec2a0d3e0152f2105b197c92bdffb5
Giovanny <didiosilva395@gmail.com>
2023-04-12 00:15:24 -0400

    Compomente controlado

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,19 +4,21 @@ import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from "react-hook-form";
+import { Controller } from 'react-hook-form';
 
 
 const newTrasactionFormSchema = z.object({
     description: z.string(),
     price: z.number(),
     category: z.string(),
-    //type: z.enum(['income', 'outcome']),
+    type: z.enum(['income', 'outcome']),
 });
 type newTransactionInput = z.infer<typeof newTrasactionFormSchema>;
 
 export function NewTransactionModal() {
 
     const {
+        control,
         register,
         handleSubmit,
         formState: { isSubmitting },
@@ -24,10 +26,10 @@ export function NewTransactionModal() {
         resolver: zodResolver(newTrasactionFormSchema),
     })
 
-   async function handleCreateNewTransaction(data: newTransactionInput) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    
-    console.log(data);
+    async function handleCreateNewTransaction(data: newTransactionInput) {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        console.log(data);
     }
     return (
         <Dialog.Portal>
@@ -41,32 +43,41 @@ export function NewTransactionModal() {
                 <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
                     <input
                         type="text"
-                        placeholder='Desrição' required 
+                        placeholder='Desrição' required
                         {...register('description')}
                     />
-                        
+
                     <input
                         type="number"
-                        placeholder='Preço' required 
-                        {...register('price',{valueAsNumber: true})}
-                        />
+                        placeholder='Preço' required
+                        {...register('price', { valueAsNumber: true })}
+                    />
                     <input
                         type="text"
-                        placeholder='Categoria' required 
+                        placeholder='Categoria' required
                         {...register('category')}
-                        />
+                    />
 
-                    <TransactionType>
-                        <TransactionTypeButton variant='income' value='income'>
-                            <ArrowCircleUp size={24} />
-                            Entrada
-                        </TransactionTypeButton>
+                    <Controller
+                        control={control}
+                        name="type"
+                        render={({field}) => {
+                            return (
+                                <TransactionType onValueChange={field.onChange} value={field.value}>
+                                    <TransactionTypeButton variant='income' value='income'>
+                                        <ArrowCircleUp size={24} />
+                                        Entrada
+                                    </TransactionTypeButton>
 
-                        <TransactionTypeButton variant='outcome' value='outcome'>
-                            <ArrowCircleDown size={24} />
-                            Entrada
-                        </TransactionTypeButton>
-                    </TransactionType>
+                                    <TransactionTypeButton variant='outcome' value='outcome'>
+                                        <ArrowCircleDown size={24} />
+                                        Entrada
+                                    </TransactionTypeButton>
+                                </TransactionType>
+                            )
+                        }}
+
+                    />
                     <button type='submit' disabled={isSubmitting}>
                         Cadastrar
                     </button>
