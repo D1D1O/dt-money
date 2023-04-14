4e2fd0b6e98afcaa197718621bb0196a7f9e9ca2
Giovanny <didiosilva395@gmail.com>
2023-04-13 20:08:43 -0400

    Usando useMemo

--- src/pages/Transactions/componentes/SearchForm/index.tsx
+++ src/pages/Transactions/componentes/SearchForm/index.tsx
@@ -5,6 +5,7 @@ import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
+import { memo } from "react";
 
 const SearchFormSchema = z.object({
   query: z.string(),
@@ -46,3 +47,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+//export const SearchForm = memo(SearchFormComponent);
